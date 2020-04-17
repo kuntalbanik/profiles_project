38f64455e88d3efdd1bba71dab5866aea4a04834
Hopeless-Developer <hopelessdeveloper@gmail.com>
2020-04-17 15:01:53 +0530

    First Commit

---
+++ profiles_api/urls.py
@@ -0,0 +1,32 @@
+"""profiles_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
+router.register('login', views.LoginViewSet, basename='login')
+router.register('feed', views.UserProfileFeedViewSet)
+
+
+urlpatterns = [
+    path('view/', views.HelloApiView.as_view(), name='view'),
+    path('', include(router.urls)),
+
+]
