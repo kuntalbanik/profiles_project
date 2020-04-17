c5ebb5cc93482fb7b72cceef00bf380a77107878
Hopeless-Developer <hopelessdeveloper@gmail.com>
2020-04-17 16:08:19 +0530

    s

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Custom user model
-AUTH_USER_MODEL = 'profiles_api.UserProfile'
+#AUTH_USER_MODEL = 'profiles_api.UserProfile'
 
 
 # Activate Django-Heroku.
